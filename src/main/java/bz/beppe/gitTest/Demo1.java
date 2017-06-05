729bef25b59eca0579c63d3220de77bf293548c8
beppezhang <beppezhang@163.com>
2017-06-05 23:08:19 +0800

    add a method

--- src/main/java/bz/beppe/gitTest/Demo1.java
+++ src/main/java/bz/beppe/gitTest/Demo1.java
@@ -5,4 +5,8 @@ public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("this is the one test");
 	}
+	
+	public void A(){
+		System.out.println("this is a commit");
+	}
 }
